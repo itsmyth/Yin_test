64f08d282087ea80cd2dfb611ec1164147893820
ismyth <ismyth@yahoo.com>
2013-02-14 10:40:02 +0000

    Initial Commit

---
+++ Yin_test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Yin_test
+//
+//  Created by Ivan Smyth on 14/02/2013.
+//  Copyright (c) 2013 Ivan Smyth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
